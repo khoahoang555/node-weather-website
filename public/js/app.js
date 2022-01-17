e99ea37a6a8bda8e6c56352d9c20d0358106ec80
Admin <khoahoangyahoo@gmail.com>
2022-01-17 08:11:56 +0700

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("this is demo file javascript!");
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
